784144f906446ee7f1c49af57364bfb42b9a5482
Paweł Wegner <pawel.wegner95@gmail.com>
2016-06-04 16:03:57 +0200

    Added Auth.

---
+++ src/Auth.cpp
@@ -0,0 +1,193 @@
+/*****************************************************************************
+ * Auth.cpp : Auth implementation
+ *
+ *****************************************************************************
+ * Copyright (C) 2016-2016 VideoLAN
+ *
+ * Authors: Paweł Wegner <pawel.wegner95@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#include "Auth.h"
+
+#include <jsoncpp/json/json.h>
+#include <microhttpd.h>
+#include <fstream>
+#include "Utility.h"
+
+const uint16_t DEFAULT_REDIRECT_URI_PORT = 12345;
+
+namespace cloudstorage {
+namespace {
+
+struct HttpServerData {
+  std::string code_;
+  std::string code_parameter_name_;
+  std::string error_parameter_name_;
+  uint16_t port_;
+  enum { Awaiting, Accepted, Denied } state_;
+};
+
+std::string sendHttpRequestFromJavaScript(const std::string& request) {
+  return "<script>\n"
+         "  var request = new XMLHttpRequest();\n"
+         "  request.open(\"GET\", \"" +
+         request +
+         "\", false);\n"
+         "  request.send(null);\n"
+         "</script>\n";
+}
+
+int httpRequestCallback(void* cls, MHD_Connection* connection,
+                        const char* /*url*/, const char* /*method*/,
+                        const char* /*version*/, const char* /*upload_data*/,
+                        size_t* /*upload_data_size*/, void** /*ptr*/) {
+  HttpServerData* data = static_cast<HttpServerData*>(cls);
+  std::string page;
+
+  const char* code = MHD_lookup_connection_value(
+      connection, MHD_GET_ARGUMENT_KIND, data->code_parameter_name_.c_str());
+  if (code) {
+    data->code_ = code;
+    page = "<body>Success.</body>" +
+           sendHttpRequestFromJavaScript("http://localhost:" +
+                                         std::to_string(data->port_) +
+                                         "/?accepted=true");
+  }
+
+  const char* error = MHD_lookup_connection_value(
+      connection, MHD_GET_ARGUMENT_KIND, data->error_parameter_name_.c_str());
+  if (error) {
+    page = "<body>Error occurred.</body>" +
+           sendHttpRequestFromJavaScript("http://localhost:" +
+                                         std::to_string(data->port_) +
+                                         "/?accepted=false");
+  }
+
+  const char* accepted = MHD_lookup_connection_value(
+      connection, MHD_GET_ARGUMENT_KIND, "accepted");
+  if (accepted) {
+    if (std::string(accepted) == "true") {
+      data->state_ = HttpServerData::Accepted;
+    } else
+      data->state_ = HttpServerData::Denied;
+  }
+
+  MHD_Response* response = MHD_create_response_from_buffer(
+      page.length(), (void*)page.c_str(), MHD_RESPMEM_MUST_COPY);
+  int ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+  MHD_destroy_response(response);
+
+  return ret;
+}
+}  // namespace
+
+Auth::Auth() : redirect_uri_port_(DEFAULT_REDIRECT_URI_PORT) {}
+
+bool Auth::authorize(IInitCallback::Pointer callback) {
+  if (!access_token() || !validateToken(*access_token())) {
+    Token::Pointer token;
+    if (access_token() && (token = refreshToken())) {
+      set_access_token(std::move(token));
+    } else {
+      if (callback) callback->userConsentRequired(*this);
+      set_authorization_code(requestAuthorizationCode());
+      set_access_token(requestAccessToken());
+    }
+  }
+  return access_token() != nullptr;
+}
+
+void Auth::set_token_data(const Json::Value& json) {
+  Token::Pointer token = make_unique<Token>();
+  token->token_ = json["access_token"].asString();
+  token->refresh_token_ = json["refresh_token"].asString();
+  token->expires_in_ = json["expires_in"].asInt();
+  set_access_token(std::move(token));
+}
+
+Json::Value Auth::token_data() const {
+  if (!access_token()) return {};
+
+  Json::Value json;
+  json["access_token"] = access_token()->token_;
+  json["refresh_token"] = access_token()->refresh_token_;
+  json["expires_in"] = access_token()->expires_in_;
+  return json;
+}
+
+const std::string& Auth::authorization_code() const {
+  return authorization_code_;
+}
+
+void Auth::set_authorization_code(const std::string& code) {
+  authorization_code_ = code;
+}
+
+const std::string& Auth::client_id() const { return client_id_; }
+
+void Auth::set_client_id(const std::string& client_id) {
+  client_id_ = client_id;
+}
+
+const std::string& Auth::client_secret() const { return client_secret_; }
+
+void Auth::set_client_secret(const std::string& client_secret) {
+  client_secret_ = client_secret;
+}
+
+std::string Auth::redirect_uri() const {
+  return std::string("http://localhost:") + std::to_string(redirect_uri_port());
+}
+
+uint16_t Auth::redirect_uri_port() const { return redirect_uri_port_; }
+
+void Auth::set_redirect_uri_port(uint16_t port) { redirect_uri_port_ = port; }
+
+Auth::Token* Auth::access_token() const { return access_token_.get(); }
+
+void Auth::set_access_token(Token::Pointer token) {
+  access_token_ = std::move(token);
+}
+
+std::string Auth::awaitAuthorizationCode(
+    std::string code_parameter_name, std::string error_parameter_name) const {
+  uint16_t http_server_port = redirect_uri_port();
+  HttpServerData data = {"", code_parameter_name, error_parameter_name,
+                         http_server_port, HttpServerData::Awaiting};
+  MHD_Daemon* http_server =
+      MHD_start_daemon(0, http_server_port, NULL, NULL, &httpRequestCallback,
+                       &data, MHD_OPTION_END);
+  fd_set rs, ws, es;
+  MHD_socket max;
+  while (data.state_ == HttpServerData::Awaiting) {
+    FD_ZERO(&rs);
+    FD_ZERO(&ws);
+    FD_ZERO(&es);
+    MHD_get_fdset(http_server, &rs, &ws, &es, &max);
+    select(max + 1, &rs, &ws, &es, NULL);
+    MHD_run_from_select(http_server, &rs, &ws, &es);
+  }
+
+  MHD_stop_daemon(http_server);
+
+  if (data.state_ == HttpServerData::Accepted)
+    return data.code_;
+  else
+    return "";
+}
+
+}  // namespace cloudstorage
---
+++ src/Auth.h
@@ -0,0 +1,71 @@
+/*****************************************************************************
+ * Auth.h : Auth prototypes
+ *
+ *****************************************************************************
+ * Copyright (C) 2016-2016 VideoLAN
+ *
+ * Authors: Paweł Wegner <pawel.wegner95@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifndef AUTH_H
+#define AUTH_H
+
+#include <jsoncpp/json/forwards.h>
+#include "IAuth.h"
+
+namespace cloudstorage {
+
+class Auth : public IAuth {
+ public:
+  Auth();
+
+  bool authorize(IInitCallback::Pointer);
+
+  void set_token_data(const Json::Value&);
+  Json::Value token_data() const;
+
+  const std::string& authorization_code() const;
+  void set_authorization_code(const std::string&);
+
+  const std::string& client_id() const;
+  void set_client_id(const std::string&);
+
+  const std::string& client_secret() const;
+  void set_client_secret(const std::string&);
+
+  std::string redirect_uri() const;
+
+  uint16_t redirect_uri_port() const;
+  void set_redirect_uri_port(uint16_t);
+
+  Token* access_token() const;
+  void set_access_token(Token::Pointer);
+
+  std::string awaitAuthorizationCode(std::string code_parameter_name,
+                                     std::string error_parameter_name) const;
+
+ private:
+  std::string authorization_code_;
+  std::string client_id_;
+  std::string client_secret_;
+  uint16_t redirect_uri_port_;
+  Token::Pointer access_token_;
+};
+
+}  // namespace cloudstorage
+
+#endif  // AUTH_H
