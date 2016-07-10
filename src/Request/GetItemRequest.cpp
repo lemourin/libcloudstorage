e71d91792cffd6a5baa7cb3bc272a745d86aaf47
Paweł Wegner <pawel.wegner95@gmail.com>
2016-07-10 12:29:07 +0200

    GetItemRequest: bugfix.

--- src/Request/GetItemRequest.cpp
+++ src/Request/GetItemRequest.cpp
@@ -53,6 +53,7 @@ GetItemRequest::GetItemRequest(std::shared_ptr<CloudProvider> p,
       node = getItem(current_request_->result(), token);
     }
     if (callback_) callback_(node);
+    result_ = node;
   });
 }
 
