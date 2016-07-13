7963d0109da225367083f90c7161b205e8e8479e
Paweł Wegner <pawel.wegner95@gmail.com>
2016-07-13 13:06:12 +0200

    Cloudbrowser: settings bugfix.

--- examples/cloudbrowser/Window.cpp
+++ examples/cloudbrowser/Window.cpp
@@ -101,7 +101,7 @@ void Window::initializeCloud(QString name) {
   }
   QString json = settings.value(name + "_hints").toString();
   QJsonObject data = QJsonDocument::fromJson(json.toLocal8Bit()).object();
-  cloud_provider_->initialize(settings.value("name").toString().toStdString(),
+  cloud_provider_->initialize(settings.value(name).toString().toStdString(),
                               make_unique<CloudProviderCallback>(this),
                               fromJson(data));
   current_directory_ = cloud_provider_->rootDirectory();
