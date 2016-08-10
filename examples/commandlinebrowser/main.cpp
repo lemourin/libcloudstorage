6642df40079cc76c8ce4986b659a28f4eb98760a
Paweł Wegner <pawel.wegner95@gmail.com>
2016-08-10 17:33:45 +0200

    Implemented simple commandline cloud browser.

---
+++ examples/commandlinebrowser/main.cpp
@@ -0,0 +1,146 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "ICloudProvider.h"
+#include "ICloudStorage.h"
+
+using cloudstorage::ICloudProvider;
+using cloudstorage::ICloudStorage;
+using cloudstorage::IItem;
+
+class Callback : public cloudstorage::ICloudProvider::ICallback {
+ public:
+  Callback(std::string drive_file) : drive_file_(drive_file) {}
+
+  Status userConsentRequired(
+      const cloudstorage::ICloudProvider& provider) override {
+    std::cout << "Required consent at url: \n";
+    std::cout << provider.authorizeLibraryUrl() << "\n";
+    return Status::WaitForAuthorizationCode;
+  }
+
+  void accepted(const cloudstorage::ICloudProvider& provider) override {
+    std::fstream file(drive_file_, std::fstream::out);
+    file << provider.token();
+  }
+
+  void declined(const cloudstorage::ICloudProvider&) override {
+    std::cout << "You denied access ;_;\n";
+  }
+
+  void error(const cloudstorage::ICloudProvider&,
+             const std::string& description) override {
+    std::cout << "Error: " << description << "\n";
+  }
+
+ private:
+  std::string drive_file_;
+};
+
+IItem::Pointer getChild(ICloudProvider::Pointer provider, IItem::Pointer item,
+                        const std::string& filename) {
+  for (auto i : provider->listDirectoryAsync(item)->result())
+    if (i->filename() == filename) return i;
+  return nullptr;
+}
+
+int main(int, char**) {
+  std::string command_line;
+  ICloudProvider::Pointer current_provider;
+  IItem::Pointer current_directory;
+  std::vector<IItem::Pointer> directory_stack;
+  std::string prompt = "/";
+  while (std::cout << prompt << ": " && std::getline(std::cin, command_line)) {
+    std::stringstream line(command_line);
+    std::string command;
+    line >> command;
+    if (command == "ls") {
+      if (current_directory == nullptr) {
+        std::cout << "Select a provider: \n";
+        for (auto p : ICloudStorage::create()->providers())
+          std::cout << p->name() << "\n";
+      } else {
+        for (auto item : current_provider->listDirectoryAsync(current_directory)
+                             ->result()) {
+          std::cout << item->filename() << "\n";
+        }
+      }
+    } else if (command == "cd") {
+      if (current_provider == nullptr) {
+        std::string provider_name;
+        line >> provider_name;
+        auto provider = ICloudStorage::create()->provider(provider_name);
+        if (!provider)
+          std::cout << "No provider with name " << provider_name << "\n";
+        else {
+          std::string filename = provider_name + ".txt";
+          std::string token;
+          std::fstream(filename, std::fstream::in) >> token;
+          provider->initialize(
+              {token,
+               std::unique_ptr<Callback>(new Callback(filename)),
+               nullptr,
+               nullptr,
+               {}});
+          prompt += provider_name + "/";
+          current_provider = provider;
+          current_directory = provider->rootDirectory();
+          directory_stack.push_back(current_directory);
+        }
+      } else {
+        std::string destination;
+        line >> destination;
+        if (destination != "..") {
+          auto item =
+              getChild(current_provider, current_directory, destination);
+          if (item) {
+            if (item->type() == IItem::FileType::Directory) {
+              current_directory = item;
+              prompt += item->filename() + "/";
+              directory_stack.push_back(item);
+            } else
+              std::cout << destination << " not a directory\n";
+          } else
+            std::cout << destination << " not found\n";
+        } else {
+          if (directory_stack.size() == 1) {
+            current_provider = nullptr;
+            current_directory = nullptr;
+            directory_stack.clear();
+            prompt = "/";
+          } else {
+            directory_stack.pop_back();
+            current_directory = directory_stack.back();
+            prompt = prompt.substr(
+                0, prompt.find_last_of('/', prompt.length() - 2) + 1);
+          }
+        }
+      }
+    } else if (command == "url") {
+      if (!current_provider)
+        std::cout << "No provider set\n";
+      else {
+        std::string file;
+        line >> std::ws;
+        std::getline(line, file);
+        std::cerr << file << "\n";
+        auto item = getChild(current_provider, current_directory, file);
+        if (item) {
+          auto data = current_provider->getItemDataAsync(item->id())->result();
+          if (data) std::cout << "Url: " << data->url() << "\n";
+        }
+      }
+    } else if (command == "help") {
+      std::cout << "ls: list directory\n"
+                << "cd: change directory\n"
+                << "url: get url to the file\n"
+                << "help: this message\n";
+    } else {
+      std::cout << "Unknown command: " << command_line << "\n";
+    }
+  }
+
+  return 0;
+}
