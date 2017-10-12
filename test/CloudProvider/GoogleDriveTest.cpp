afb8b0c32c3b7e1a77886f8f346e8a9a7e65315d
Paweł Wegner <pawel.wegner95@gmail.com>
2017-10-13 01:13:19 +0200

    test: added sample GoogleDrive unit test.

--- test/CloudProvider/GoogleDriveTest.cpp
+++ test/CloudProvider/GoogleDriveTest.cpp
@@ -20,6 +20,51 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
+#include <json/json.h>
+#include "ICloudStorage.h"
+#include "Utility/HttpMock.h"
+#include "Utility/Utility.h"
 #include "gtest/gtest.h"
 
-TEST(GoogleDriveTest, ListDirectoryTest) { ASSERT_EQ(0, 0); }
+using namespace cloudstorage;
+using ::testing::_;
+using ::testing::AtLeast;
+using ::testing::Return;
+using ::testing::InvokeArgument;
+using ::testing::Invoke;
+using ::testing::WithArgs;
+
+class GoogleDriveTest : public ::testing::Test {
+ public:
+  void SetUp() {}
+
+  void TearDown() {}
+};
+
+ACTION(CallSend) {
+  Json::Value json;
+  Json::Value item;
+  item["kind"] = "drive#file";
+  item["name"] = "test";
+  json["files"].append(item);
+  *arg2 << json;
+  arg0(IHttpRequest::Response{IHttpRequest::Ok, {}, arg2, arg3});
+}
+
+TEST_F(GoogleDriveTest, ListDirectoryTest) {
+  ICloudProvider::InitData data;
+  data.http_engine_ = util::make_unique<HttpMock>();
+  const HttpMock& http = static_cast<const HttpMock&>(*data.http_engine_);
+  auto provider = ICloudStorage::create()->provider("google", std::move(data));
+  auto request = std::make_shared<HttpRequestMock>();
+  EXPECT_CALL(*request, setParameter(_, _)).Times(AtLeast(1));
+  EXPECT_CALL(*request, setHeaderParameter(_, _)).Times(AtLeast(1));
+  EXPECT_CALL(*request, send(_, _, _, _, _)).WillOnce(CallSend());
+  EXPECT_CALL(http,
+              create("https://www.googleapis.com/drive/v3/files", "GET", true))
+      .WillRepeatedly(Return(request));
+  auto r = provider->listDirectoryAsync(provider->rootDirectory())->result();
+  ASSERT_NE(r.right(), nullptr);
+  ASSERT_EQ(r.right()->size(), 1);
+  ASSERT_EQ(r.right()->front()->filename(), "test");
+}
