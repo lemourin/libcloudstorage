123b41b822fe7b1467d6145bbb3c95c36b21ec53
Paweł Wegner <pawel.wegner95@gmail.com>
2017-10-31 15:26:32 +0100

    cloudbrowser: added android files.

---
+++ examples/cloudbrowser/android/cloudbrowser.pro
@@ -0,0 +1,111 @@
+TEMPLATE = app
+CONFIG += c++11
+QT += quick webview androidextras svg
+
+DEFINES += WITH_THUMBNAILER WITH_CURL WITH_CRYPTOPP WITH_MEGA WITH_MICROHTTPD
+
+ANDROID_TOOLCHAIN_PATH = $$(ANDROID_TOOLCHAIN_PATH)
+
+isEmpty(ANDROID_TOOLCHAIN_PATH) {
+    error(ANDROID_TOOLCHAIN_PATH environment variable not set.)
+}
+
+QMAKE_CXX = $$ANDROID_TOOLCHAIN_PATH/bin/clang++
+QMAKE_CXXFLAGS =
+QMAKE_LINK = $$QMAKE_CXX
+QMAKE_INCDIR_POST =
+QMAKE_LIBS_PRIVATE =
+
+INCLUDEPATH = \
+    ../../../src/ \
+    $$ANDROID_TOOLCHAIN_PATH/include \
+    $$ANDROID_TOOLCHAIN_PATH/include/c++/4.9.x \
+    $$ANDROID_TOOLCHAIN_PATH/sysroot/usr/include
+
+LIBS += \
+    -L$$ANDROID_TOOLCHAIN_PATH/lib \
+    -L$$ANDROID_TOOLCHAIN_PATH/arm-linux-androideabi/lib/ \
+    -lc++_shared -lcurl -lmicrohttpd -ljsoncpp -ltinyxml2 -lcryptopp -lmega -lffmpegthumbnailer
+
+SOURCES += \
+    ../main.cpp \
+    ../CloudContext.cpp \
+    ../GenerateThumbnail.cpp \
+    ../Exec.cpp \
+    ../FileDialog.cpp \
+    ../File.cpp
+
+SOURCES += \
+    ../../../src/Utility/CloudStorage.cpp \
+    ../../../src/Utility/Auth.cpp \
+    ../../../src/Utility/Item.cpp \
+    ../../../src/Utility/FileSystem.cpp \
+    ../../../src/Utility/Utility.cpp \
+    ../../../src/CloudProvider/CloudProvider.cpp \
+    ../../../src/CloudProvider/GoogleDrive.cpp \
+    ../../../src/CloudProvider/OneDrive.cpp \
+    ../../../src/CloudProvider/Dropbox.cpp \
+    ../../../src/CloudProvider/AmazonS3.cpp \
+    ../../../src/CloudProvider/Box.cpp \
+    ../../../src/CloudProvider/YouTube.cpp \
+    ../../../src/CloudProvider/YandexDisk.cpp \
+    ../../../src/CloudProvider/WebDav.cpp \
+    ../../../src/CloudProvider/PCloud.cpp \
+    ../../../src/CloudProvider/HubiC.cpp \
+    ../../../src/CloudProvider/GooglePhotos.cpp \
+    ../../../src/Request/Request.cpp \
+    ../../../src/Request/HttpCallback.cpp \
+    ../../../src/Request/AuthorizeRequest.cpp \
+    ../../../src/Request/DownloadFileRequest.cpp \
+    ../../../src/Request/GetItemRequest.cpp \
+    ../../../src/Request/ListDirectoryRequest.cpp \
+    ../../../src/Request/ListDirectoryPageRequest.cpp \
+    ../../../src/Request/UploadFileRequest.cpp \
+    ../../../src/Request/GetItemDataRequest.cpp \
+    ../../../src/Request/DeleteItemRequest.cpp \
+    ../../../src/Request/CreateDirectoryRequest.cpp \
+    ../../../src/Request/MoveItemRequest.cpp \
+    ../../../src/Request/RenameItemRequest.cpp \
+    ../../../src/Request/ExchangeCodeRequest.cpp \
+    ../../../src/Request/GetItemUrlRequest.cpp \
+    ../../../src/Request/RecursiveRequest.cpp \
+    ../../../src/CloudProvider/MegaNz.cpp \
+    ../../../src/Utility/CryptoPP.cpp \
+    ../../../src/Utility/CurlHttp.cpp \
+    ../../../src/Utility/MicroHttpdServer.cpp
+
+RESOURCES += \
+    ../resources.qrc
+
+DISTFILES += \
+    AndroidManifest.xml \
+    Workaround.qml \
+    src/org/videolan/cloudbrowser/CloudBrowser.java
+
+HEADERS += \
+    ../CloudContext.h \
+    ../FileDialog.h \
+    ../File.h
+
+ANDROID_PACKAGE_SOURCE_DIR = $$PWD
+
+ANDROID_EXTRA_LIBS = \
+    $$ANDROID_TOOLCHAIN_PATH/arm-linux-androideabi/lib/libc++_shared.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libjsoncpp.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libmicrohttpd.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libcurl.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libtinyxml2.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libcares.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libnghttp2.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libsqlite3.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libmega.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libcryptopp.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libavdevice.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libavfilter.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libswscale.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libavformat.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libavcodec.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libswresample.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libavutil.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libpng16.so \
+    $$ANDROID_TOOLCHAIN_PATH/lib/libffmpegthumbnailer.so
---
+++ examples/cloudbrowser/android/src/org/videolan/cloudbrowser/CloudBrowser.java
@@ -0,0 +1,122 @@
+package org.videolan.cloudbrowser;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.provider.OpenableColumns;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class CloudBrowser extends org.qtproject.qt5.android.bindings.QtActivity {
+    private static NotificationManager m_notificationManager;
+    private static Notification.Builder m_builder;
+    private static CloudBrowser m_instance;
+
+    public static class FileOutput {
+        public ParcelFileDescriptor m_descriptor;
+        public FileOutputStream m_stream;
+    }
+
+    public static class FileInput {
+        public ParcelFileDescriptor m_descriptor;
+        public FileInputStream m_stream;
+    }
+
+    public CloudBrowser() {
+        m_instance = this;
+    }
+
+    public static Intent openFileDialog() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        return intent;
+    }
+
+    public static Intent createFileDialog(String filename) {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.putExtra(Intent.EXTRA_TITLE, filename);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        return intent;
+    }
+
+    public static FileOutput openWrite(String uri) throws IOException {
+        FileOutput file = new FileOutput();
+        file.m_descriptor = m_instance.getContentResolver().openFileDescriptor(Uri.parse(uri.replace(" ", "%20")), "w");
+        file.m_stream = new FileOutputStream(file.m_descriptor.getFileDescriptor());
+        return file;
+    }
+
+    public static FileInput openRead(String uri) throws IOException {
+        FileInput file = new FileInput();
+        file.m_descriptor = m_instance.getContentResolver().openFileDescriptor(Uri.parse(uri.replace(" ", "%20")), "r");
+        file.m_stream = new FileInputStream(file.m_descriptor.getFileDescriptor());
+        return file;
+    }
+
+    public static void write(FileOutput file, byte[] data) throws IOException {
+        file.m_stream.write(data);
+    }
+
+    public static byte[] read(FileInput file, int size) throws IOException {
+        byte[] buffer = new byte[size];
+        int nsize = file.m_stream.read(buffer);
+        if (nsize == -1)
+            return new byte[0];
+        byte[] result = new byte[nsize];
+        for (int i = 0; i < nsize; i++)
+            result[i] = buffer[i];
+        return result;
+    }
+
+    public static String filename(Uri uri) {
+        Cursor cursor = m_instance.getContentResolver().query(uri, null, null, null, null, null);
+        String result = "";
+        try {
+            if (cursor != null && cursor.moveToFirst())
+                result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+        } finally {
+            cursor.close();
+        }
+        return result;
+    }
+
+    public static long size(FileInput file) throws IOException {
+        return file.m_descriptor.getStatSize();
+    }
+
+    public static boolean reset(FileInput file) throws IOException {
+        file.m_stream.close();
+        file.m_stream = new FileInputStream(file.m_descriptor.getFileDescriptor());
+        return true;
+    }
+
+    public static void closeWrite(FileOutput file) throws IOException {
+        file.m_stream.close();
+        file.m_descriptor.close();
+    }
+
+    public static void closeRead(FileInput file) throws IOException {
+        file.m_stream.close();
+        file.m_descriptor.close();
+    }
+
+    public static void notify(String content_text, String title) {
+        if (m_notificationManager == null) {
+            m_notificationManager = (NotificationManager)m_instance.getSystemService(Context.NOTIFICATION_SERVICE);
+            m_builder = new Notification.Builder(m_instance);
+            m_builder.setSmallIcon(R.drawable.icon);
+            m_builder.setContentTitle(title);
+        }
+
+        m_builder.setContentText(content_text);
+        m_notificationManager.notify(1, m_builder.build());
+    }
+}
