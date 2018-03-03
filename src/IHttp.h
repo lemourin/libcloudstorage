565966b3ebe3d20a89c6716c804c7ebdb7e5b07e
Paweł Wegner <pawel.wegner95@gmail.com>
2018-03-03 12:52:11 +0100

    IHttp: added code 500 constant.

--- src/IHttp.h
+++ src/IHttp.h
@@ -59,6 +59,7 @@ class IHttpRequest {
   static constexpr int Forbidden = 403;
   static constexpr int NotFound = 404;
   static constexpr int RangeInvalid = 416;
+  static constexpr int InternalServerError = 500;
   static constexpr int ServiceUnavailable = 503;
   static constexpr int Aborted = 600;
   static constexpr int Unknown = 700;
