e3bee4d28d2e776cb1bae7c48da0236c0ed24b53
Paweł Wegner <pawel.wegner95@gmail.com>
2018-03-05 17:35:46 +0100

    cloudbrowser: refactored thumbnailer code.

--- bin/cloudbrowser/src/GenerateThumbnail.cpp
+++ bin/cloudbrowser/src/GenerateThumbnail.cpp
@@ -41,39 +41,31 @@ extern "C" {
 namespace cloudstorage {
 
 const int THUMBNAIL_SIZE = 256;
-const int MAX_RETRY_COUNT = 10;
+const int MAX_RETRY_COUNT = 24;
 
 namespace {
 
-template <class T>
-class UniquePtr {
- public:
-  UniquePtr(T* pointer, std::function<void(T**)> deleter)
-      : pointer_(pointer), deleter_(deleter) {}
-  ~UniquePtr() {
-    if (pointer_) deleter_(&pointer_);
-  }
+struct ImageSize {
+  int width_;
+  int height_;
+};
 
-  T* operator->() const { return pointer_; }
-
-  T*& get() { return pointer_; }
-
- private:
-  T* pointer_;
-  std::function<void(T**)> deleter_;
+struct CallbackData {
+  std::function<bool(std::chrono::system_clock::time_point)> interrupt_;
+  std::chrono::system_clock::time_point start_time_;
 };
 
 template <class T>
-using Pointer = UniquePtr<T>;
+using Pointer = std::unique_ptr<T, std::function<void(T*)>>;
 
 template <class T>
 Pointer<T> make(T* d, std::function<void(T**)> f) {
-  return Pointer<T>(d, f);
+  return Pointer<T>(d, [=](T* d) { f(&d); });
 }
 
 template <class T>
 Pointer<T> make(T* d, std::function<void(T*)> f) {
-  return Pointer<T>(d, [=](T** d) { f(*d); });
+  return Pointer<T>(d, f);
 }
 
 std::string av_error(int err) {
@@ -86,6 +78,134 @@ std::string av_error(int err) {
 
 void check(int code, const std::string& call) {
   if (code < 0) throw std::logic_error(call + " (" + av_error(code) + ")");
+}
+
+Pointer<AVFormatContext> create_format_context(
+    const std::string& url,
+    std::function<bool(std::chrono::system_clock::time_point)> interrupt) {
+  av_register_all();
+  av_log_set_level(AV_LOG_PANIC);
+  check(avformat_network_init(), "avformat_network_init");
+  auto context = avformat_alloc_context();
+  auto data = new CallbackData{interrupt, std::chrono::system_clock::now()};
+  context->interrupt_callback.opaque = data;
+  context->interrupt_callback.callback = [](void* t) -> int {
+    auto d = reinterpret_cast<CallbackData*>(t);
+    return d->interrupt_(d->start_time_);
+  };
+  int e = 0;
+  if ((e = avformat_open_input(&context, url.c_str(), nullptr, nullptr)) < 0) {
+    delete data;
+    check(e, "avformat_open_input");
+  } else if ((e = avformat_find_stream_info(context, nullptr)) < 0) {
+    avformat_close_input(&context);
+    delete data;
+    check(e, "avformat_find_stream_info");
+  }
+  return make<AVFormatContext>(context, [data](AVFormatContext* d) {
+    avformat_close_input(&d);
+    delete data;
+  });
+}
+
+Pointer<AVCodecContext> create_codec_context(AVFormatContext* context) {
+  auto idx =
+      av_find_best_stream(context, AVMEDIA_TYPE_VIDEO, -1, -1, nullptr, 0);
+  check(idx, "av_find_best_stream");
+  auto codec = avcodec_find_decoder(context->streams[idx]->codecpar->codec_id);
+  if (!codec) throw std::logic_error("decoder not found");
+  auto codec_context =
+      make<AVCodecContext>(avcodec_alloc_context3(codec), avcodec_free_context);
+  check(avcodec_parameters_to_context(codec_context.get(),
+                                      context->streams[idx]->codecpar),
+        "avcodec_parameters_to_context");
+  check(avcodec_open2(codec_context.get(), codec, nullptr), "avcodec_open2");
+  return codec_context;
+}
+
+Pointer<AVPacket> create_packet() {
+  auto packet = new AVPacket;
+  av_init_packet(packet);
+  return Pointer<AVPacket>(packet, [](AVPacket* packet) {
+    av_packet_unref(packet);
+    delete packet;
+  });
+}
+
+Pointer<AVFrame> decode_frame(AVFormatContext* context,
+                              AVCodecContext* codec_context) {
+  int retry_count = 0;
+  Pointer<AVFrame> result_frame;
+  while (retry_count < MAX_RETRY_COUNT && !result_frame) {
+    auto packet = create_packet();
+    auto read_packet = av_read_frame(context, packet.get());
+    if (read_packet != 0) {
+      retry_count++;
+      continue;
+    }
+    auto send_packet = avcodec_send_packet(codec_context, packet.get());
+    if (send_packet != 0) {
+      retry_count++;
+      continue;
+    }
+    auto frame = make<AVFrame>(av_frame_alloc(), av_frame_free);
+    auto code = avcodec_receive_frame(codec_context, frame.get());
+    if (code == 0)
+      result_frame = std::move(frame);
+    else if (code != -EAGAIN)
+      check(code, "avcodec_receive_frame");
+  }
+  if (!result_frame) throw std::logic_error("failed to fetch frame");
+  return result_frame;
+}
+
+std::string encode_frame(AVFrame* frame) {
+  auto png_codec = avcodec_find_encoder(AV_CODEC_ID_PNG);
+  if (!png_codec) throw std::logic_error("png codec not found");
+  auto png_context = make<AVCodecContext>(avcodec_alloc_context3(png_codec),
+                                          avcodec_free_context);
+  png_context->time_base = {1, 24};
+  png_context->pix_fmt = AVPixelFormat(frame->format);
+  png_context->width = frame->width;
+  png_context->height = frame->height;
+  check(avcodec_open2(png_context.get(), png_codec, nullptr), "avcodec_open2");
+  check(avcodec_send_frame(png_context.get(), frame), "avcodec_send_frame");
+  auto packet = create_packet();
+  check(avcodec_receive_packet(png_context.get(), packet.get()),
+        "avcodec_receive_packet");
+  return std::string(reinterpret_cast<char*>(packet->data), packet->size);
+}
+
+Pointer<AVFrame> create_rgb_frame(AVCodecContext* codec_context, AVFrame* frame,
+                                  ImageSize size) {
+  auto format = AV_PIX_FMT_RGBA;
+  auto sws_context = make<SwsContext>(
+      sws_getContext(codec_context->width, codec_context->height,
+                     codec_context->pix_fmt, size.width_, size.height_, format,
+                     SWS_BICUBIC, nullptr, nullptr, nullptr),
+      sws_freeContext);
+  auto rgb_frame = make<AVFrame>(av_frame_alloc(), av_frame_free);
+  rgb_frame->format = format;
+  rgb_frame->width = size.width_;
+  rgb_frame->height = size.height_;
+  check(av_image_alloc(rgb_frame->data, rgb_frame->linesize, frame->width,
+                       frame->height, format, 32),
+        "av_image_alloc");
+  check(sws_scale(sws_context.get(), frame->data, frame->linesize, 0,
+                  frame->height, rgb_frame->data, rgb_frame->linesize),
+        "sws_scale");
+  return make<AVFrame>(rgb_frame.release(), [=](AVFrame* f) {
+    av_freep(&f->data);
+    av_frame_free(&f);
+  });
+}
+
+ImageSize thumbnail_size(const ImageSize& i, int target) {
+  if (i.width_ > i.height_) {
+    return {target, i.height_ * target / i.width_};
+  } else {
+    return {i.height_ * target / i.width_, target};
+  }
 }
 
 }  // namespace
@@ -102,107 +222,16 @@ EitherError<std::string> generate_thumbnail(
 #endif
     const auto length = strlen(file);
     if (url.substr(0, length) == file) effective_url = url.substr(length);
-    av_register_all();
-    check(avformat_network_init(), "avformat_network_init");
-    auto context =
-        make<AVFormatContext>(avformat_alloc_context(), avformat_free_context);
-    struct CallbackData {
-      std::function<bool(std::chrono::system_clock::time_point)> interrupt_;
-      std::chrono::system_clock::time_point start_time_;
-    } data = {interrupt, std::chrono::system_clock::now()};
-    context->interrupt_callback.opaque = &data;
-    context->interrupt_callback.callback = [](void* t) -> int {
-      auto d = reinterpret_cast<CallbackData*>(t);
-      return d->interrupt_(d->start_time_);
-    };
-    auto network_guard = make<std::nullptr_t>(
-        nullptr, [](std::nullptr_t*) { avformat_network_deinit(); });
-    check(avformat_open_input(&context.get(), effective_url.c_str(), nullptr,
-                              nullptr),
-          "avformat_open_input");
-    auto input_guard =
-        make<AVFormatContext*>(&context.get(), avformat_close_input);
-    check(avformat_find_stream_info(context.get(), nullptr),
-          "avformat_find_stream_info");
-    auto idx = av_find_best_stream(context.get(), AVMEDIA_TYPE_VIDEO, -1, -1,
-                                   nullptr, 0);
-    check(idx, "av_find_best_stream");
-    auto codec =
-        avcodec_find_decoder(context->streams[idx]->codecpar->codec_id);
-    if (!codec) throw std::logic_error("decoder not found");
-    auto codec_context = make<AVCodecContext>(avcodec_alloc_context3(codec),
-                                              avcodec_free_context);
-    check(avcodec_parameters_to_context(codec_context.get(),
-                                        context->streams[idx]->codecpar),
-          "avcodec_parameters_to_context");
-    check(avcodec_open2(codec_context.get(), codec, nullptr), "avcodec_open2");
-    check(av_seek_frame(context.get(), -1, context->duration / 2, 0),
+    auto context = create_format_context(effective_url, interrupt);
+    check(av_seek_frame(context.get(), -1, context->duration / 10, 0),
           "av_seek_frame");
-    auto frame = make<AVFrame>(av_frame_alloc(), av_frame_free);
-    AVPacket packet;
-    av_init_packet(&packet);
-    int retry_count = 0;
-    bool generated_frame = false;
-    while (!interrupt(data.start_time_) && retry_count < MAX_RETRY_COUNT &&
-           !generated_frame) {
-      check(av_read_frame(context.get(), &packet), "av_read_frame");
-      auto send_packet = avcodec_send_packet(codec_context.get(), &packet);
-      if (send_packet != 0) {
-        retry_count++;
-        continue;
-      }
-      auto code = avcodec_receive_frame(codec_context.get(), frame.get());
-      if (code == 0)
-        generated_frame = true;
-      else if (code != -EAGAIN)
-        check(code, "avcodec_receive_frame");
-    }
-    if (!generated_frame) throw std::logic_error("failed to fetch frame");
-    int thumbnail_width, thumbnail_height;
-    if (codec_context->width > codec_context->height) {
-      thumbnail_width = THUMBNAIL_SIZE;
-      thumbnail_height =
-          codec_context->height * THUMBNAIL_SIZE / codec_context->width;
-    } else {
-      thumbnail_width =
-          codec_context->height * THUMBNAIL_SIZE / codec_context->width;
-      thumbnail_height = THUMBNAIL_SIZE;
-    }
-    auto sws_context = make<SwsContext>(
-        sws_getContext(codec_context->width, codec_context->height,
-                       codec_context->pix_fmt, thumbnail_width,
-                       thumbnail_height, AV_PIX_FMT_RGBA, SWS_BICUBIC, nullptr,
-                       nullptr, nullptr),
-        sws_freeContext);
-    auto rgb_frame = make<AVFrame>(av_frame_alloc(), av_frame_free);
-    rgb_frame->format = AV_PIX_FMT_RGBA;
-    rgb_frame->width = thumbnail_width;
-    rgb_frame->height = thumbnail_height;
-    check(av_image_alloc(rgb_frame->data, rgb_frame->linesize, frame->width,
-                         frame->height, AV_PIX_FMT_RGBA, 32),
-          "av_image_alloc");
-    auto image_data_guard = make<std::nullptr_t>(
-        nullptr, [&](std::nullptr_t*) { av_freep(&rgb_frame->data); });
-    check(sws_scale(sws_context.get(), frame->data, frame->linesize, 0,
-                    frame->height, rgb_frame->data, rgb_frame->linesize),
-          "sws_scale");
-    auto png_codec = avcodec_find_encoder(AV_CODEC_ID_PNG);
-    if (!png_codec) throw std::logic_error("png codec not found");
-    auto png_context = make<AVCodecContext>(avcodec_alloc_context3(png_codec),
-                                            avcodec_free_context);
-    png_context->time_base = {1, 24};
-    png_context->pix_fmt = AV_PIX_FMT_RGBA;
-    png_context->width = thumbnail_width;
-    png_context->height = thumbnail_height;
-    check(avcodec_open2(png_context.get(), png_codec, nullptr),
-          "avcodec_open2");
-    check(avcodec_send_frame(png_context.get(), rgb_frame.get()),
-          "avcodec_send_frame");
-    check(avcodec_receive_packet(png_context.get(), &packet),
-          "avcodec_receive_packet");
-    std::stringstream output;
-    output.write(reinterpret_cast<char*>(packet.data), packet.size);
-    return output.str();
+    auto codec_context = create_codec_context(context.get());
+    auto frame = decode_frame(context.get(), codec_context.get());
+    auto rgb_frame = create_rgb_frame(
+        codec_context.get(), frame.get(),
+        thumbnail_size({codec_context->width, codec_context->height},
+                       THUMBNAIL_SIZE));
+    return encode_frame(rgb_frame.get());
   } catch (const std::exception& e) {
     return Error{IHttpRequest::Failure, e.what()};
   }
