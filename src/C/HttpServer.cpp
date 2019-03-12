c5fa29448427fe70956ff0f94e2251a3dfd06834
Paweł Wegner <pawel.wegner95@gmail.com>
2019-03-12 11:57:16 +0100

    C/HttpServer: implemented http server interface wrapper.

--- src/C/HttpServer.cpp
+++ src/C/HttpServer.cpp
@@ -30,64 +30,54 @@ using namespace cloudstorage;
 namespace {
 class HttpServer : public IHttpServer {
  public:
-  HttpServer(cloud_http_server_callback *callback,
-             cloud_http_server_release release, void *data)
-      : callback_(
-            *reinterpret_cast<IHttpServer::ICallback::Pointer *>(callback)),
-        release_(release),
-        data_(data) {}
+  HttpServer(cloud_http_server *server, cloud_http_server_operations ops,
+             ICallback::Pointer callback)
+      : http_server_(server), operations_(ops), callback_(callback) {}
 
-  ~HttpServer() { release_(data_); }
+  ~HttpServer() override { operations_.release_http_server(http_server_); }
 
   ICallback::Pointer callback() const override { return callback_; }
-  void *data() const { return data_; }
 
  private:
+  cloud_http_server *http_server_;
+  cloud_http_server_operations operations_;
   ICallback::Pointer callback_;
-  cloud_http_server_release release_;
-  void *data_;
 };
 }  // namespace
 
 cloud_http_server_factory *cloud_http_server_factory_create(
-    cloud_http_server_create_callback callback, void *data) {
+    cloud_http_server_operations *d, void *data) {
   class HttpServerFactory : public IHttpServerFactory {
    public:
-    HttpServerFactory(cloud_http_server_create_callback callback, void *data)
-        : callback_(callback), data_(data) {}
+    HttpServerFactory(cloud_http_server_operations ops, void *data)
+        : operations_(ops), data_(data) {}
+
+    ~HttpServerFactory() override { operations_.release(data_); }
 
     IHttpServer::Pointer create(IHttpServer::ICallback::Pointer callback,
                                 const std::string &session_id,
                                 IHttpServer::Type type) override {
-      return std::unique_ptr<IHttpServer>(
-          reinterpret_cast<IHttpServer *>(callback_(
-              data_, reinterpret_cast<cloud_http_server_callback *>(&callback),
-              session_id.c_str(), static_cast<int>(type))));
+      auto cserver = operations_.create(
+          reinterpret_cast<cloud_http_server_callback *>(callback.get()),
+          session_id.c_str(), static_cast<int>(type), data_);
+      return util::make_unique<HttpServer>(cserver, operations_, callback);
     }
 
    private:
-    cloud_http_server_create_callback callback_;
+    cloud_http_server_operations operations_;
     void *data_;
   };
   return reinterpret_cast<cloud_http_server_factory *>(
-      new std::unique_ptr<IHttpServerFactory>(
-          util::make_unique<HttpServerFactory>(callback, data)));
+      util::make_unique<HttpServerFactory>(*d, data).release());
 }
 
 void cloud_http_server_factory_release(cloud_http_server_factory *d) {
-  delete reinterpret_cast<std::unique_ptr<IHttpServerFactory> *>(d);
+  delete reinterpret_cast<IHttpServerFactory *>(d);
 }
 
 cloud_http_server_factory *cloud_http_server_factory_default_create() {
   return reinterpret_cast<cloud_http_server_factory *>(
-      new std::unique_ptr<IHttpServerFactory>(IHttpServerFactory::create()));
-}
-
-cloud_http_server *cloud_http_server_create(
-    cloud_http_server_callback *callback, cloud_http_server_release release,
-    void *data) {
-  return reinterpret_cast<cloud_http_server *>(
-      new HttpServer(callback, release, data));
+      IHttpServerFactory::create().release());
 }
 
 cloud_http_server_response *cloud_http_server_handle(
@@ -97,4 +87,103 @@ cloud_http_server_response *cloud_http_server_handle(
       server->callback()
           ->handle(*reinterpret_cast<IHttpServer::IRequest *>(request))
           .release());
+}
+
+cloud_http_server_request *cloud_http_server_request_create(
+    cloud_http_server_request_operations *ops, void *userdata) {
+  struct HttpResponse : public IHttpServer::IResponse {
+    HttpResponse(cloud_http_server_request_operations ops,
+                 cloud_http_server_response *response,
+                 IResponse::ICallback::Pointer callback)
+        : operations_(ops),
+          response_(response),
+          callback_(std::move(callback)) {}
+
+    ~HttpResponse() override { operations_.release_response(response_); }
+
+    void resume() override { operations_.resume(response_); }
+
+    void completed(CompletedCallback cb) override {
+      operations_.complete(
+          response_,
+          [](const void *param) {
+            auto cb = reinterpret_cast<const CompletedCallback *>(param);
+            (*cb)();
+            delete cb;
+          },
+          new CompletedCallback(cb));
+    }
+
+    cloud_http_server_request_operations operations_;
+    cloud_http_server_response *response_;
+    IResponse::ICallback::Pointer callback_;
+  };
+
+  struct HttpRequest : public IHttpServer::IRequest {
+    HttpRequest(cloud_http_server_request_operations ops, void *userdata)
+        : operations_(ops), userdata_(userdata) {}
+
+    ~HttpRequest() override { operations_.release(userdata_); }
+
+    const char *get(const std::string &name) const override {
+      cloud_string *str = operations_.get(name.c_str(), userdata_);
+      storage_ = str;
+      free(const_cast<char *>(str));
+      return storage_.c_str();
+    }
+    const char *header(const std::string &name) const override {
+      cloud_string *str = operations_.header(name.c_str(), userdata_);
+      storage_ = str;
+      free(const_cast<char *>(str));
+      return storage_.c_str();
+    }
+    std::string method() const override {
+      cloud_string *str = operations_.method(userdata_);
+      std::string result = str;
+      free(const_cast<char *>(str));
+      return result;
+    }
+    std::string url() const override {
+      cloud_string *str = operations_.url(userdata_);
+      std::string result = str;
+      free(const_cast<char *>(str));
+      return result;
+    }
+
+    IHttpServer::IResponse::Pointer response(
+        int code, const IHttpServer::IResponse::Headers &headers, int size,
+        IHttpServer::IResponse::ICallback::Pointer callback) const override {
+      cloud_http_server_response *r = operations_.response(
+          code,
+          reinterpret_cast<const cloud_http_server_response_headers *>(
+              &headers),
+          size,
+          reinterpret_cast<cloud_http_server_response_callback *>(
+              callback.get()));
+      return util::make_unique<HttpResponse>(operations_, r,
+                                             std::move(callback));
+    }
+
+    cloud_http_server_request_operations operations_;
+    void *userdata_;
+    mutable std::string storage_;
+  };
+  return reinterpret_cast<cloud_http_server_request *>(
+      new HttpRequest(*ops, userdata));
+}
+
+int cloud_http_server_response_callback_put_data(
+    cloud_http_server_response_callback *callback, uint8_t *data, size_t size) {
+  return reinterpret_cast<IHttpServer::IResponse::ICallback *>(callback)
+      ->putData(reinterpret_cast<char *>(data), size);
+}
+
+void cloud_http_server_response_headers_iterate(
+    cloud_http_server_response_headers *d,
+    void (*callback)(cloud_string *, cloud_string *, void *), void *userdata) {
+  const auto &headers =
+      *reinterpret_cast<const IHttpServer::IResponse::Headers *>(d);
+  for (const auto &p : headers) {
+    callback(p.first.c_str(), p.second.c_str(), userdata);
+  }
 }
--- src/C/HttpServer.h
+++ src/C/HttpServer.h
@@ -25,30 +25,68 @@
 
 #include "Item.h"
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif  // __cplusplus
 
+#define cloud_http_server_response_callback_suspend 0
+#define cloud_http_server_response_callback_abort -1
+#define cloud_http_server_response_callback_end -2
+
 struct cloud_http_server_factory;
 struct cloud_http_server_response;
-struct cloud_http_server_request;
+struct cloud_http_server_response_callback;
+struct cloud_http_server_response_headers;
 struct cloud_http_server_callback;
 struct cloud_http_server;
 
-typedef void (*cloud_http_server_release)(void*);
-typedef struct cloud_http_server* (*cloud_http_server_create_callback)(
-    void* data, struct cloud_http_server_callback*, cloud_string* session,
-    int type);
+struct cloud_http_server_request_operations {
+  void (*release)(void* userdata);
+  cloud_string* (*get)(cloud_string* name, void* userdata);
+  cloud_string* (*header)(cloud_string* name, void* userdata);
+  cloud_string* (*method)(void* userdata);
+  cloud_string* (*url)(void* userdata);
+  struct cloud_http_server_response* (*response)(
+      int code, const struct cloud_http_server_response_headers*, int size,
+      struct cloud_http_server_response_callback*);
 
-CLOUDSTORAGE_API struct cloud_http_server_factory* cloud_http_server_factory_default_create();
-CLOUDSTORAGE_API struct cloud_http_server_factory* cloud_http_server_factory_create(
-    cloud_http_server_create_callback, void* data);
-CLOUDSTORAGE_API void cloud_http_server_factory_release(struct cloud_http_server_factory*);
+  void (*release_response)(struct cloud_http_server_response*);
+  void (*resume)(struct cloud_http_server_response*);
+  void (*complete)(struct cloud_http_server_response*,
+                   void (*callback)(const void* param), const void* param);
+};
 
-CLOUDSTORAGE_API struct cloud_http_server* cloud_http_server_create(
-    struct cloud_http_server_callback*, cloud_http_server_release, void* data);
+struct cloud_http_server_operations {
+  void (*release)(void* userdata);
+  void (*release_http_server)(struct cloud_http_server*);
+  struct cloud_http_server* (*create)(struct cloud_http_server_callback*,
+                                      cloud_string* session, int type,
+                                      void* userdata);
+};
+
+CLOUDSTORAGE_API struct cloud_http_server_factory*
+cloud_http_server_factory_default_create();
+CLOUDSTORAGE_API struct cloud_http_server_factory*
+cloud_http_server_factory_create(struct cloud_http_server_operations*,
+                                 void* data);
+CLOUDSTORAGE_API void cloud_http_server_factory_release(
+    struct cloud_http_server_factory*);
+
+CLOUDSTORAGE_API struct cloud_http_server_request*
+cloud_http_server_request_create(struct cloud_http_server_request_operations*,
+                                 void* userdata);
+
 CLOUDSTORAGE_API struct cloud_http_server_response* cloud_http_server_handle(
     struct cloud_http_server*, struct cloud_http_server_request*);
+CLOUDSTORAGE_API int cloud_http_server_response_callback_put_data(
+    struct cloud_http_server_response_callback*, uint8_t* data, size_t size);
+
+CLOUDSTORAGE_API void cloud_http_server_response_headers_iterate(
+    struct cloud_http_server_response_headers*,
+    void (*callback)(cloud_string* key, cloud_string* value, void* userdata),
+    void* userdata);
 
 #ifdef __cplusplus
 }
