b794316ba3382ffb2c1d0f3886f463903391e102
Paweł Wegner <pawel.wegner95@gmail.com>
2019-05-13 21:42:23 +0200

    AnimeZone: updated mp4upload descrambling regex.

--- src/CloudProvider/AnimeZone.cpp
+++ src/CloudProvider/AnimeZone.cpp
@@ -345,7 +345,7 @@ std::string extract_url(const std::string &page) {
   uint64_t arg_a = uint64_t(std::atoll(match[2].str().c_str()));
   uint64_t arg_c = uint64_t(std::atoll(match[3].str().c_str()));
   std::string unpacked = unpack_js(match[1].str(), arg_a, arg_c, arg_k);
-  re::regex source_rx("\"file\":\"([^\"]*)\"");
+  re::regex source_rx("\"(https:\\/\\/\\S*.mp4)\"");
   re::smatch source_match;
   if (!re::regex_search(unpacked, source_match, source_rx)) {
     throw std::logic_error(util::Error::COULD_NOT_FIND_MP4_URL);
