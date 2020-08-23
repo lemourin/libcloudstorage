75fda4fd5cd4c45881f6c5a21765fbb24f841b6c
Paweł Wegner <pawel.wegner95@gmail.com>
2020-08-23 17:33:01 +0200

    RequestTest: added DoesOneReauthorizationForParallelRequests test.

--- test/Utility/RequestTest.cpp
+++ test/Utility/RequestTest.cpp
@@ -14,6 +14,7 @@ using ::testing::MockFunction;
 using ::testing::Pointee;
 using ::testing::Property;
 using ::testing::Return;
+using ::testing::SaveArg;
 
 TEST(RequestTest, ResolvesResult) {
   using ReturnValue = EitherError<std::string>;
@@ -100,6 +101,86 @@ TEST(RequestTest, DoesReauthorization) {
 
   EXPECT_THAT(request->run()->result().right(), Pointee(Eq("test")));
   EXPECT_EQ(request_count, 2);
+}
+
+TEST(RequestTest, DoesOneReauthorizationForParallelRequests) {
+  using ReturnValue = EitherError<std::string>;
+
+  auto provider = CloudProviderMock::create();
+
+  MockFunction<void(std::shared_ptr<Request<ReturnValue>>)> first_resolver;
+  MockFunction<void(ReturnValue)> first_callback;
+  auto first_request = std::make_shared<Request<ReturnValue>>(
+      provider, first_callback.AsStdFunction(), first_resolver.AsStdFunction());
+
+  MockFunction<void(std::shared_ptr<Request<ReturnValue>>)> second_resolver;
+  MockFunction<void(ReturnValue)> second_callback;
+  auto second_request = std::make_shared<Request<ReturnValue>>(
+      provider, second_callback.AsStdFunction(),
+      second_resolver.AsStdFunction());
+
+  IHttpRequest::CompleteCallback refresh_token_request_complete;
+
+  EXPECT_CALL(*provider->auth(), refreshTokenRequest)
+      .Times(1)
+      .WillOnce(Return([&refresh_token_request_complete] {
+        auto mock_request = std::make_shared<HttpRequestMock>();
+        EXPECT_CALL(*mock_request, send)
+            .WillOnce(SaveArg<0>(&refresh_token_request_complete));
+        return mock_request;
+      }()));
+  EXPECT_CALL(*provider->auth(), refreshTokenResponse)
+      .WillOnce(Return(ByMove(std::make_unique<IAuth::Token>())));
+
+  int first_request_count = 0;
+
+  EXPECT_CALL(first_resolver, Call(first_request))
+      .WillOnce(Invoke([=, &first_request_count](
+                           const std::shared_ptr<Request<ReturnValue>>& r) {
+        r->request(
+            [=, &first_request_count](const util::Output&) {
+              auto mock_request = std::make_shared<HttpRequestMock>();
+              EXPECT_CALL(*mock_request, send)
+                  .WillOnce(InvokeArgument<0>(IHttpRequest::Response{
+                      first_request_count == 0 ? 401 : 242}));
+              first_request_count++;
+              return mock_request;
+            },
+            [=](const EitherError<Response>& e) {
+              ASSERT_NE(e.right(), nullptr);
+              EXPECT_THAT(e.right()->http_code(), Eq(242));
+              first_request->done(std::string("test1"));
+            });
+      }));
+
+  int second_request_count = 0;
+
+  EXPECT_CALL(second_resolver, Call(second_request))
+      .WillOnce(Invoke([=, &second_request_count](
+                           const std::shared_ptr<Request<ReturnValue>>& r) {
+        r->request(
+            [=, &second_request_count](const util::Output&) {
+              auto mock_request = std::make_shared<HttpRequestMock>();
+              EXPECT_CALL(*mock_request, send)
+                  .WillOnce(InvokeArgument<0>(IHttpRequest::Response{
+                      second_request_count == 0 ? 401 : 243}));
+              second_request_count++;
+              return mock_request;
+            },
+            [=](const EitherError<Response>& e) {
+              ASSERT_NE(e.right(), nullptr);
+              EXPECT_THAT(e.right()->http_code(), Eq(243));
+              second_request->done(std::string("test2"));
+            });
+      }));
+
+  auto first_wrapper = first_request->run();
+  auto second_wrapper = second_request->run();
+
+  refresh_token_request_complete(IHttpRequest::Response{200});
+
+  EXPECT_THAT(first_wrapper->result().right(), Pointee(Eq("test1")));
+  EXPECT_THAT(second_wrapper->result().right(), Pointee(Eq("test2")));
 }
 
 }  // namespace cloudstorage
