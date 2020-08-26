703be5af4d589559958086fdd2a2f01e177ca596
Paweł Wegner <pawel.wegner95@gmail.com>
2020-08-26 11:36:38 +0200

    MegaNzTest: added GetsGeneralData test.

--- test/CloudProvider/MegaNzTest.cpp
+++ test/CloudProvider/MegaNzTest.cpp
@@ -2,22 +2,22 @@
 
 #include "gtest/gtest.h"
 
+#include <json/json.h>
 #include <future>
 
 #include "ICloudAccess.h"
 #include "TestData.h"
 #include "Utility/CloudFactoryMock.h"
+#include "Utility/Utility.h"
 
 namespace cloudstorage {
 
 using ::testing::DoAll;
 using ::testing::ElementsAre;
-using ::testing::HasSubstr;
 using ::testing::Invoke;
-using ::testing::InvokeArgument;
 using ::testing::Pointee;
 using ::testing::Property;
-using ::testing::Return;
+using ::testing::WithArg;
 using ::testing::WithArgs;
 
 const std::string TOKEN(reinterpret_cast<const char*>(mega_token_txt),
@@ -31,28 +31,65 @@ const std::string FILES_RESPONSE(
     reinterpret_cast<const char*>(mega_files_response_json),
     mega_files_response_json_len);
 
-auto ImmediatelyRespond(const std::string& string) {
-  return DoAll(WithArgs<0, 1, 2, 3>(
-      Invoke([=](const IHttpRequest::CompleteCallback& on_completed,
-                 const std::shared_ptr<std::istream>& input_stream,
-                 const std::shared_ptr<std::ostream>& output_stream,
-                 const std::shared_ptr<std::ostream>& error_stream) {
+std::shared_ptr<HttpRequestMock> MockedMegaResponse(const std::string& url) {
+  auto request = std::make_shared<HttpRequestMock>();
+  EXPECT_CALL(*request, send)
+      .WillRepeatedly(DoAll(WithArgs<
+                            0, 1, 2,
+                            3>(Invoke([=](const IHttpRequest::CompleteCallback&
+                                              on_completed,
+                                          const std::shared_ptr<std::istream>&
+                                              input_stream,
+                                          const std::shared_ptr<std::ostream>&
+                                              output_stream,
+                                          const std::shared_ptr<std::ostream>&
+                                              error_stream) {
         IHttpRequest::Response response;
         response.http_code_ = 200;
         response.output_stream_ = output_stream;
         response.error_stream_ = error_stream;
-        response.headers_.insert(
-            {"content-length", std::to_string(string.length())});
 
-        *output_stream << string;
+        std::stringstream sstream;
+        sstream << input_stream->rdbuf();
+
+        std::string output;
+
+        if (url.find("https://g.api.mega.co.nz/cs") != std::string::npos) {
+          auto json = util::json::from_string(sstream.str());
+          if (json[0]["a"] == "us") {
+            output = LOGIN_RESPONSE;
+          } else if (json[0]["a"] == "f") {
+            output = FILES_RESPONSE;
+          } else if (json[0]["a"] == "log") {
+            output = "[0]";
+          } else if (json[0]["a"] == "uq") {
+            output =
+                R"([{"mstrg":53687091200,"cstrgn":{"UIZ2wIzS":[3210730732,424,7,8340824,1],"0Npw3ZzD":[0,0,0,0,0],"UEhzlbJA":[0,0,0,0,0]},"cstrg":3210730732,"uslw":9000,"srvratio":25.000381475547417}])";
+          }
+        } else if (
+            url ==
+            R"(https://g.api.mega.co.nz/wsc?sn=kT5h5dzLGbI&sid=3Lpm9xrLONJ2W7CwGN9LLEVkbkljbmdRU1ZNi4ck5FBxxE4E_d99A20_tA)") {
+          output =
+              R"({"w":"https://g.api.mega.co.nz/wsc/_pkb7Bs-cdSjYs6bB9s11Q","sn":"XqlH_atPIrU"})";
+        } else if (
+            url ==
+            R"(https://g.api.mega.co.nz/wsc?c=50&sid=3Lpm9xrLONJ2W7CwGN9LLEVkbkljbmdRU1ZNi4ck5FBxxE4E_d99A20_tA)") {
+          output = R"({"c":[],"lsn":"S0eXeaePFAA"})";
+        } else if (
+            url ==
+            R"(https://g.api.mega.co.nz/wsc/_pkb7Bs-cdSjYs6bB9s11Q?sn=XqlH_atPIrU&sid=3Lpm9xrLONJ2W7CwGN9LLEVkbkljbmdRU1ZNi4ck5FBxxE4E_d99A20_tA)") {
+          return;
+        }
+
+        ASSERT_FALSE(output.empty());
+
+        response.headers_.insert(
+            {"content-length", std::to_string(output.length())});
+
+        *output_stream << output;
 
         on_completed(response);
-      })));
-}
-
-std::shared_ptr<HttpRequestMock> ImmediateRequest(const std::string& string) {
-  auto request = std::make_shared<HttpRequestMock>();
-  EXPECT_CALL(*request, send).WillRepeatedly(ImmediatelyRespond(string));
+      }))));
   return request;
 }
 
@@ -60,33 +97,8 @@ TEST(MegaNzTest, ListsDirectory) {
   auto mock = CloudFactoryMock::create();
   auto provider = mock.factory()->create("mega", {TOKEN});
 
-  EXPECT_CALL(*mock.http(),
-              create(HasSubstr("https://g.api.mega.co.nz/cs"), "POST", true))
-      .WillOnce(Return(ImmediateRequest(LOGIN_RESPONSE)))
-      .WillOnce(Return(ImmediateRequest(FILES_RESPONSE)))
-      .WillOnce(Return(ImmediateRequest("[0]")));
-
-  EXPECT_CALL(
-      *mock.http(),
-      create(
-          R"(https://g.api.mega.co.nz/wsc?sn=kT5h5dzLGbI&sid=3Lpm9xrLONJ2W7CwGN9LLEVkbkljbmdRU1ZNi4ck5FBxxE4E_d99A20_tA)",
-          "POST", true))
-      .WillOnce(Return(ImmediateRequest(
-          R"({"w":"https://g.api.mega.co.nz/wsc/_pkb7Bs-cdSjYs6bB9s11Q","sn":"XqlH_atPIrU"})")));
-
-  EXPECT_CALL(
-      *mock.http(),
-      create(
-          R"(https://g.api.mega.co.nz/wsc?c=50&sid=3Lpm9xrLONJ2W7CwGN9LLEVkbkljbmdRU1ZNi4ck5FBxxE4E_d99A20_tA)",
-          "POST", true))
-      .WillOnce(Return(ImmediateRequest(R"({"c":[],"lsn":"S0eXeaePFAA"})")));
-
-  EXPECT_CALL(
-      *mock.http(),
-      create(
-          R"(https://g.api.mega.co.nz/wsc/_pkb7Bs-cdSjYs6bB9s11Q?sn=XqlH_atPIrU&sid=3Lpm9xrLONJ2W7CwGN9LLEVkbkljbmdRU1ZNi4ck5FBxxE4E_d99A20_tA)",
-          "POST", true))
-      .WillRepeatedly(Return(std::make_shared<HttpRequestMock>()));
+  EXPECT_CALL(*mock.http(), create)
+      .WillRepeatedly(WithArg<0>(Invoke(MockedMegaResponse)));
 
   std::promise<EitherError<IItem::List>> semaphore;
   auto request =
@@ -113,6 +125,30 @@ TEST(MegaNzTest, ListsDirectory) {
           Pointee(Property(&IItem::filename,
                            "STEINS;GATE 0 Ending Full LAST GAME by Zwei.mp4")),
           Pointee(Property(&IItem::filename, "docker_clean.sh")))));
+}
+
+TEST(MegaNzTest, GetsGeneralData) {
+  auto mock = CloudFactoryMock::create();
+  auto provider = mock.factory()->create("mega", {TOKEN});
+
+  EXPECT_CALL(*mock.http(), create)
+      .WillRepeatedly(WithArg<0>(Invoke(MockedMegaResponse)));
+
+  std::promise<EitherError<GeneralData>> semaphore;
+  auto request =
+      provider->generalData()
+          .then([&](const GeneralData& d) { semaphore.set_value(d); })
+          .error<IException>([&](const auto& e) {
+            semaphore.set_value(Error{e.code(), e.what()});
+          });
+
+  EitherError<GeneralData> result = semaphore.get_future().get();
+
+  ASSERT_NE(result.right(), nullptr);
+
+  EXPECT_EQ(result.right()->username_, "lemourin@gmail.com");
+  EXPECT_EQ(result.right()->space_total_, 53687091200);
+  EXPECT_EQ(result.right()->space_used_, 3210730732);
 }
 
 }  // namespace cloudstorage
