8e21d76b51bbda96b77504c3c992460c36f7aabd
Paweł Wegner <pawel.wegner95@gmail.com>
2020-10-18 10:48:29 +0200

    test: move CloudFactoryMock implementation to .cpp.

---
+++ test/Utility/CloudFactoryMock.cpp
@@ -0,0 +1,114 @@
+#include "CloudFactoryMock.h"
+
+using ::testing::ByMove;
+using ::testing::Invoke;
+using ::testing::InvokeArgument;
+using ::testing::Return;
+
+namespace cloudstorage {
+void PrintTo(const std::shared_ptr<Error>& error, std::ostream* os) {
+  if (error) {
+    *os << "Error(" << error->code_ << ", " << error->description_ << ")";
+  } else {
+    *os << "Error(nullptr)";
+  }
+}
+void PrintTo(const IItem& item, std::ostream* os) {
+  *os << "Item(id = " << item.id() << ", filename = " << item.filename() << ")";
+}
+void PrintTo(IItem::FileType type, std::ostream* os) {
+  const auto str = [=] {
+    switch (type) {
+      case IItem::FileType::Image:
+        return "Image";
+      case IItem::FileType::Audio:
+        return "Audio";
+      case IItem::FileType::Video:
+        return "Video";
+      case IItem::FileType::Directory:
+        return "Directory";
+      default:
+        return "Unknown";
+    }
+  }();
+  *os << "FileType(" << str << ")";
+}
+}  // namespace cloudstorage
+
+namespace std::chrono {
+void PrintTo(std::chrono::system_clock::time_point timestamp,
+             std::ostream* os) {
+  *os << "TimeStamp(" << std::chrono::system_clock::to_time_t(timestamp) << ")";
+}
+}  // namespace std::chrono
+
+CloudFactoryMock::CloudFactoryMock(
+    HttpMock* http, HttpServerFactoryMock* http_server_factory,
+    ThreadPoolFactoryMock* thread_pool_factory_mock,
+    CloudFactoryCallbackMock* callback,
+    std::unique_ptr<cloudstorage::ICloudFactory> factory)
+    : http_(http),
+      http_server_factory_(http_server_factory),
+      thread_pool_factory_mock_(thread_pool_factory_mock),
+      callback_(callback),
+      factory_(std::move(factory)) {}
+
+CloudFactoryMock CloudFactoryMock::create() {
+  auto http_mock = std::make_unique<HttpMock>();
+  auto http_server_factory_mock = std::make_unique<HttpServerFactoryMock>();
+  auto thread_pool_factory_mock = std::make_unique<ThreadPoolFactoryMock>();
+  auto callback_mock = std::make_unique<CloudFactoryCallbackMock>();
+
+  auto http_mock_ptr = http_mock.get();
+  auto http_server_factory_mock_ptr = http_server_factory_mock.get();
+  auto thread_pool_factory_mock_ptr = thread_pool_factory_mock.get();
+  auto callback_mock_ptr = callback_mock.get();
+
+  CloudFactoryMock result(
+      http_mock_ptr, http_server_factory_mock_ptr, thread_pool_factory_mock_ptr,
+      callback_mock_ptr,
+      cloudstorage::ICloudFactory::create(cloudstorage::ICloudFactory::InitData{
+          "http://cloudstorage-test/", std::move(http_mock),
+          std::move(http_server_factory_mock), nullptr,
+          std::move(thread_pool_factory_mock), std::move(callback_mock)}));
+
+  EXPECT_CALL(*result.callback_, onEventsAdded)
+      .WillRepeatedly(Invoke(
+          [factory = result.factory_.get()] { factory->processEvents(); }));
+
+  auto default_thread_pool = std::make_unique<ThreadPoolMock>();
+  ON_CALL(*default_thread_pool, schedule).WillByDefault(InvokeArgument<0>());
+  ON_CALL(*result.thread_pool_factory(), create)
+      .WillByDefault(Return(ByMove(std::move(default_thread_pool))));
+
+  return result;
+}
+
+HttpMock* CloudFactoryMock::http() const { return http_; }
+
+HttpServerFactoryMock* CloudFactoryMock::http_server() const {
+  return http_server_factory_;
+}
+
+ThreadPoolFactoryMock* CloudFactoryMock::thread_pool_factory() const {
+  return thread_pool_factory_mock_;
+}
+
+CloudFactoryCallbackMock* CloudFactoryMock::callback() const {
+  return callback_;
+}
+
+cloudstorage::ICloudFactory* CloudFactoryMock::factory() const {
+  return factory_.get();
+}
+
+void ExpectImmediatePromise(cloudstorage::Promise<>&& promise) {
+  cloudstorage::EitherError<void> result;
+  bool promise_invoked = false;
+  promise.then([&] { promise_invoked = true; })
+      .error<cloudstorage::IException>([&](const auto& e) {
+        result = cloudstorage::Error{e.code(), e.what()};
+      });
+  EXPECT_TRUE(promise_invoked);
+  EXPECT_EQ(result.left(), nullptr);
+}
--- test/Utility/CloudFactoryMock.h
+++ test/Utility/CloudFactoryMock.h
@@ -10,40 +10,13 @@
 #include "ThreadPoolMock.h"
 
 namespace cloudstorage {
-inline void PrintTo(const std::shared_ptr<Error>& error, std::ostream* os) {
-  if (error) {
-    *os << "Error(" << error->code_ << ", " << error->description_ << ")";
-  } else {
-    *os << "Error(nullptr)";
-  }
-}
-inline void PrintTo(const IItem& item, std::ostream* os) {
-  *os << "Item(id = " << item.id() << ", filename = " << item.filename() << ")";
-}
-inline void PrintTo(IItem::FileType type, std::ostream* os) {
-  const auto str = [=] {
-    switch (type) {
-      case IItem::FileType::Image:
-        return "Image";
-      case IItem::FileType::Audio:
-        return "Audio";
-      case IItem::FileType::Video:
-        return "Video";
-      case IItem::FileType::Directory:
-        return "Directory";
-      default:
-        return "Unknown";
-    }
-  }();
-  *os << "FileType(" << str << ")";
-}
+void PrintTo(const std::shared_ptr<Error>& error, std::ostream* os);
+void PrintTo(const IItem& item, std::ostream* os);
+void PrintTo(IItem::FileType type, std::ostream* os);
 }  // namespace cloudstorage
 
 namespace std::chrono {
-inline void PrintTo(std::chrono::system_clock::time_point timestamp,
-                    std::ostream* os) {
-  *os << "TimeStamp(" << std::chrono::system_clock::to_time_t(timestamp) << ")";
-}
+void PrintTo(std::chrono::system_clock::time_point timestamp, std::ostream* os);
 }  // namespace std::chrono
 
 class CloudFactoryCallbackMock : public cloudstorage::ICloudFactory::ICallback {
@@ -62,60 +35,19 @@ class CloudFactoryCallbackMock : public cloudstorage::ICloudFactory::ICallback {
 
 class CloudFactoryMock {
  public:
-  static CloudFactoryMock create() {
-    auto http_mock = std::make_unique<HttpMock>();
-    auto http_server_factory_mock = std::make_unique<HttpServerFactoryMock>();
-    auto thread_pool_factory_mock = std::make_unique<ThreadPoolFactoryMock>();
-    auto callback_mock = std::make_unique<CloudFactoryCallbackMock>();
+  static CloudFactoryMock create();
 
-    auto http_mock_ptr = http_mock.get();
-    auto http_server_factory_mock_ptr = http_server_factory_mock.get();
-    auto thread_pool_factory_mock_ptr = thread_pool_factory_mock.get();
-    auto callback_mock_ptr = callback_mock.get();
-
-    CloudFactoryMock result(
-        http_mock_ptr, http_server_factory_mock_ptr,
-        thread_pool_factory_mock_ptr, callback_mock_ptr,
-        cloudstorage::ICloudFactory::create(
-            cloudstorage::ICloudFactory::InitData{
-                "http://cloudstorage-test/", std::move(http_mock),
-                std::move(http_server_factory_mock), nullptr,
-                std::move(thread_pool_factory_mock),
-                std::move(callback_mock)}));
-
-    EXPECT_CALL(*result.callback_, onEventsAdded)
-        .WillRepeatedly(testing::Invoke(
-            [factory = result.factory_.get()] { factory->processEvents(); }));
-
-    auto default_thread_pool = std::make_unique<ThreadPoolMock>();
-    ON_CALL(*default_thread_pool, schedule)
-        .WillByDefault(testing::InvokeArgument<0>());
-    ON_CALL(*result.thread_pool_factory(), create)
-        .WillByDefault(
-            testing::Return(testing::ByMove(std::move(default_thread_pool))));
-
-    return result;
-  }
-
-  HttpMock* http() const { return http_; }
-  HttpServerFactoryMock* http_server() const { return http_server_factory_; }
-  ThreadPoolFactoryMock* thread_pool_factory() const {
-    return thread_pool_factory_mock_;
-  }
-  CloudFactoryCallbackMock* callback() const { return callback_; }
-  cloudstorage::ICloudFactory* factory() const { return factory_.get(); }
+  HttpMock* http() const;
+  HttpServerFactoryMock* http_server() const;
+  ThreadPoolFactoryMock* thread_pool_factory() const;
+  CloudFactoryCallbackMock* callback() const;
+  cloudstorage::ICloudFactory* factory() const;
 
  private:
   CloudFactoryMock(HttpMock* http, HttpServerFactoryMock* http_server_factory,
                    ThreadPoolFactoryMock* thread_pool_factory_mock,
                    CloudFactoryCallbackMock* callback,
-                   std::unique_ptr<cloudstorage::ICloudFactory> factory)
-      : http_(http),
-        http_server_factory_(http_server_factory),
-        thread_pool_factory_mock_(thread_pool_factory_mock),
-        callback_(callback),
-        factory_(std::move(factory)) {}
-
+                   std::unique_ptr<cloudstorage::ICloudFactory> factory);
   HttpMock* http_;
   HttpServerFactoryMock* http_server_factory_;
   ThreadPoolFactoryMock* thread_pool_factory_mock_;
@@ -135,16 +67,7 @@ void ExpectImmediatePromise(cloudstorage::Promise<Result>&& promise,
   EXPECT_THAT(result.right(), testing::Pointee(matcher));
 }
 
-inline void ExpectImmediatePromise(cloudstorage::Promise<>&& promise) {
-  cloudstorage::EitherError<void> result;
-  bool promise_invoked = false;
-  promise.then([&] { promise_invoked = true; })
-      .template error<cloudstorage::IException>([&](const auto& e) {
-        result = cloudstorage::Error{e.code(), e.what()};
-      });
-  EXPECT_TRUE(promise_invoked);
-  EXPECT_EQ(result.left(), nullptr);
-}
+void ExpectImmediatePromise(cloudstorage::Promise<>&& promise);
 
 template <typename Result, typename ErrorMatcher>
 void ExpectFailedPromise(cloudstorage::Promise<Result>&& promise,
